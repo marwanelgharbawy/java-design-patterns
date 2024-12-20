ddc2f80dd4b08fd0f3c227d86249aeddffb2f1f4
Marwan <marwanelgharbawy2@gmail.com>
2024-12-20 20:10:17 +0200

    Decorator: Create base component interface

---
+++ src/decorator/pizza/PizzaI.java
@@ -0,0 +1,6 @@
+package decorator.pizza;
+
+public interface PizzaI {
+    String getDescription(); // returns a description of the pizza
+    double getCost();        // returns the cost of the pizza
+}
