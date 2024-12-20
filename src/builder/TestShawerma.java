ef3ee836d480cd9b3e1a3591d5d610b6cc063117
Marwan <marwanelgharbawy2@gmail.com>
2024-12-20 18:20:03 +0200

    Builder: Create an empty constructor, and a setBuilder method

--- src/builder/TestShawerma.java
+++ src/builder/TestShawerma.java
@@ -1,4 +1,19 @@
 package builder;
 
+import builder.shawerma.*;
+
 public class TestShawerma {
+    public static void main(String[] args) {
+        Waiter waiter = new Waiter();
+        ShawermaBuilder chickenShawermaBuilder = new ChickenShawermaBuilder();
+        ShawermaBuilder beefShawermaBuilder = new BeefShawermaBuilder();
+
+        waiter.setBuilder(chickenShawermaBuilder);
+        Shawerma chickenShawerma = waiter.constructShawerma();
+        System.out.println(chickenShawerma);
+
+        waiter.setBuilder(beefShawermaBuilder);
+        Shawerma beefShawerma = waiter.constructShawerma();
+        System.out.println(beefShawerma);
+    }
 }
